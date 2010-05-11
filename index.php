a0a79b59930034b45d231674691d58b30d944f94
TheSavior <github@eli-white.com>
2010-05-10 23:18:07 -0700

    Test Git

--- index.php
+++ index.php
@@ -93,3 +93,16 @@ $registry->display = new Library_Display($registry);
 $registry->display->parse();
 
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
+
+<html>
+<head>
+  <meta name="generator" content=
+  "HTML Tidy for Windows (vers 14 February 2006), see www.w3.org">
+
+  <title></title>
+</head>
+
+<body>
+</body>
+</html>
