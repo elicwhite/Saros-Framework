884d0e5d449a3dfe25d162d4b3d85657b2f33e2d
TheSavior <github@eli-white.com>
2010-05-18 12:12:33 -0700

    Added command line support for tests.

--- Tests/index.php
+++ Tests/index.php
@@ -1,4 +1,3 @@
-<pre>
 <?php
 
 /**
@@ -13,14 +12,7 @@
  * @link http://github.com/TheSavior/Saros-Framework
  */
 
-// Add PHPUnit to the include path
-$incPath = get_include_path();
-set_include_path("/mounted-storage/home45c/sub001/sc21473-GRUR" . PATH_SEPARATOR . $incPath);
-
 error_reporting(E_ALL);
-
-// Let execution time be infinite
-set_time_limit(0);
 
 // Require PHPUnit Files
 require_once 'PHPUnit/Framework.php';
@@ -44,4 +36,3 @@ Saros_Suite::main();
 //}
 
 ?>
-</pre>
