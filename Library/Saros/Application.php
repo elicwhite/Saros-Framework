5372db9c3a30b3333d0562ba9bf5c5e32455fbb5
TheSavior <github@eli-white.com>
2013-02-20 10:51:11 -0800

    Moved a lot from index.php into a new Saros\Application class.

---
+++ Library/Saros/Application.php
@@ -0,0 +1,68 @@
+<?php
+namespace Saros;
+
+/**
+ *
+ * @copyright Eli White & SaroSoftware 2010
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPL
+ *
+ * @package SarosFramework
+ * @author Eli White
+ * @link http://sarosoftware.com
+ * @link http://github.com/TheSavior/Saros-Framework
+ */
+class Application
+{
+    public static function run()
+    {
+        set_exception_handler(array('\Saros\Exception\Handler', 'handle'));
+        
+        /*
+        Create an output buffer. This is being used
+        so that we can at any point clear all output.
+        For example; our exception handler
+        does not display anything other than the exception
+        message.
+        */
+        ob_start();
+
+        // Create a new registry of variables
+        $GLOBALS["registry"] = new \Saros\Core\Registry();
+        
+        // Load up the core set of utilities
+        $GLOBALS["registry"]->utils = new \Saros\Core\Utilities();
+
+        // Create a new registry object to be used for configuration
+        $GLOBALS["registry"]->config  = new \Saros\Core\Registry();
+
+        // Load the router
+        $GLOBALS["registry"]->router = new \Saros\Core\Router();
+
+        $GLOBALS["registry"]->display = \Saros\Display::getInstance($GLOBALS["registry"]);
+
+        // Get the current route
+        $GLOBALS["registry"]->router->parseRoute();
+
+        // We want to setup our application
+        \Application\Setup::doSetup($GLOBALS["registry"]);
+
+        // Calls the module's setup file
+        $GLOBALS["registry"]->router->setupModule();
+               
+        // Creates an instance of the class that will be
+        // Called to generate our page
+        $GLOBALS["registry"]->router->createInstance($GLOBALS["registry"]);
+
+        /**
+         * Sets the view. This can be changed
+         * at any time before the class is run
+         */
+        $GLOBALS["registry"]->router->getInstance()->setView($GLOBALS["registry"]->display);
+               
+        // Run the controller
+        $GLOBALS["registry"]->router->run();
+                    
+        // Display our page
+        $GLOBALS["registry"]->display->parse();
+    }
+}
--- index.php
+++ index.php
@@ -16,57 +16,7 @@ error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', 'on');
 
 define("ROOT_PATH",  realpath(dirname(__FILE__))."/");
-
-
-
+     
 require 'vendor/autoload.php';
 
-set_exception_handler(array('Saros\Exception\Handler', 'handle'));
-
-/*
-Create an output buffer. This is being used
-so that we can at any point clear all output.
-For example; our exception handler
-does not display anything other than the exception
-message.
-*/
-ob_start();
-
-// Create a new registry of variables
-$registry = new \Saros\Core\Registry();
-
-// Load up the core set of utilities
-$registry->utils = new \Saros\Core\Utilities();
-
-// Create a new registry object to be used for configuration
-$registry->config  = new \Saros\Core\Registry();
-
-// Load the router
-$registry->router = new \Saros\Core\Router();
-
-$registry->display = \Saros\Display::getInstance($registry);
-
-// Get the current route
-$registry->router->parseRoute();
-
-// We want to setup our application
-\Application\Setup::doSetup($registry);
-
-// Calls the module's setup file
-$registry->router->setupModule();
-       
-// Creates an instance of the class that will be
-// Called to generate our page
-$registry->router->createInstance($registry);
-
-/**
- * Sets the view. This can be changed
- * at any time before the class is run
- */
-$registry->router->getInstance()->setView($registry->display);
-       
-// Run the controller
-$registry->router->run();
-            
-// Display our page
-$registry->display->parse();
+\Saros\Application::run();
