d2d6f90f0a46f436b5305e02c3364c335f6c334f
TheSavior <github@eli-white.com>
2013-03-02 20:17:31 -0800

    Beginning Integration with travis-ci.org

--- Tests/init.php
+++ Tests/init.php
@@ -1,67 +1,20 @@
 <?php
 
 /**
- * Our Test Runner
- *
- * @copyright Eli White & SaroSoftware 2010
- * @license http://www.gnu.org/licenses/gpl.html GNU GPL
- *
- * @package SarosFramework
- * @author Eli White
- * @link http://sarosoftware.com
- * @link http://github.com/TheSavior/Saros-Framework
- */
-
-error_reporting(E_ALL);
-
-define("ROOT_PATH",  realpath(dirname(dirname(__FILE__)))."/");
-
-// This is needed to allow us to load fixtures
-function autoload($classname)
-{
-    // Skip PHPUnit files
-    //if(false !== strpos($classname, 'PHPUnit_')) {
-    //    return false;
-    //}
-    
-    $parts = explode("_",$classname);
-
-	$fileLocation = implode('/',$parts).".php";
-    
-    //echo $fileLocation;
-    /*if (false !== strpos($classname, "Fixture")){
-        die(getcwd()."\\".$fileLocation);
-        die(var_dump(file_exists($fileLocation)));
-        require_once($fileLocation);
-    }*/
-    
-    if (file_exists("Tests/".$fileLocation))
-        require_once("Tests/".$fileLocation);
-    // We want to check for named libraries
-    // It is a named library when $parts[0] matches a folder in Library
-    else if(is_dir("Library/".$parts[0]))
-        require_once("Library/".$fileLocation);
-    else
-        // We don't know where this class exists. Maybe there is another autoloader that does
-		return false;
+* Autoload test fixtures
+*/
+function test_autoloader($className) {
+    // Only autoload classes that start with "Test_" and "Entity_"
+    if(false === strpos($className, 'Test_')) {
+        return false;
+    }
+    $classFile = str_replace('_', '/', $className) . '.php';
+    require __DIR__ . '/' . $classFile;
 }
-spl_autoload_register('autoload');
+spl_autoload_register('test_autoloader');
 
-
-require_once(ROOT_PATH.'Library/Saros/Core/AutoLoader.php');
-spl_autoload_register(array('Saros\Core\AutoLoader', 'autoload'));
 
 // I don't like calling this here. I'm not quite sure how to solve this
 Saros\Session::start();
-
-// Include the spot tests                               
-//require_once(ROOT_PATH."Library/Spot/Tests/AllTests.php");
-//Spot_Tests::main();
-//echo "\n\n";
-
-// Run the Saros Framework tests
-
-
-
 
 ?>
