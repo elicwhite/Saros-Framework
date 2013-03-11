fdd301c088169f43a950abbf0142bbffc360ab68
TheSavior <github@eli-white.com>
2013-03-11 01:19:43 -0700

    Updating tests and autoload scripts.

--- tests/init.php
+++ tests/init.php
@@ -2,28 +2,5 @@
 
 define("ROOT_PATH",  realpath(dirname(dirname(__FILE__)))."/");
 require ROOT_PATH.'vendor/autoload.php';
-  
-/**
-* Autoload test fixtures
-*/
-function test_autoloader($className) {
-    // Only autoload classes that start with "Test" and "Fixture"
-    if(false === strpos($className, 'Test') && false === strpos($className, 'Fixture')) {
-        return false;
-    }
-    
-    $classFile = $className . '.php';
-    $classFile = str_replace("\\", "/", $classFile);
-    $file = __DIR__ . '/' . $classFile;
-    
-    if (file_exists($file)) {
-        require_once $file;
-    }                      
-}
-spl_autoload_register('test_autoloader');
-
-
-// I don't like calling this here. I'm not quite sure how to solve this
-//\Saros\Session::start();
 
 ?>
